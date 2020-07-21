e76862c477115633ddd372003b55e411dc71ce58
seanmcquaid <sean.mcquaid2@verizon.net>
2020-07-20 21:01:09 -0400

    finished text input component

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,3 +1,32 @@
+import { type } from 'os';
 import React from 'react';
 import styled from 'styled-components';
 
+type TextInputProps = {
+  onChange: (event: React.SyntheticEvent) => void;
+  title: string;
+  placeholder: string;
+  value: string;
+  ref: React.RefObject<HTMLInputElement> | null;
+};
+
+const TextInput: React.FC<TextInputProps> = ({ onChange, title, placeholder, value, ref }) => (
+  <TextInputContainer>
+    <StyledLabel>{title}</StyledLabel>
+    <StyledInput
+      value={value}
+      placeholder={placeholder}
+      ref={ref}
+      onChange={onChange}
+      data-testid={`${title}TextInput`}
+    />
+  </TextInputContainer>
+);
+
+const TextInputContainer = styled.div``;
+
+const StyledLabel = styled.label``;
+
+const StyledInput = styled.input``;
+
+export default TextInput
