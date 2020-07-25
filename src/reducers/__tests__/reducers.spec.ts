969f9b912ed270ca0b35522018d723dd4a58941e
seanmcquaid <sean.mcquaid2@verizon.net>
2020-07-24 22:03:22 -0400

    finished reducer test

--- src/reducers/__tests__/reducers.spec.ts
+++ src/reducers/__tests__/reducers.spec.ts
@@ -1,9 +1,30 @@
+import rootReducer from '../index';
+import Actions from 'actions/types';
+
 describe('rootReducer', () => {
   it('setLocation', () => {
+    const state = {
+      selectedLocation: {
+        entity_id: '',
+        title: '',
+      },
+    };
 
+    const action = {
+      type: Actions.setLocation,
+      payload: {
+        selectedLocation: {
+          entity_id: '',
+          title: '',
+        },
+      },
+    };
+    expect(rootReducer(state, action)).toEqual({
+      selectedLocation: {
+        entity_id: '',
+        title: '',
+      },
+    });
   });
 
-  it('default', () => {
-
-  });
 });
