f35448bb5a29b0d5f57675c134d93f7828a39cbe
seanmcquaid <sean.mcquaid2@verizon.net>
2020-07-27 21:51:22 -0400

    more or less sketched out the request needed for the final page, should finish this tonight

--- src/pages/RestaurantListPage/index.tsx
+++ src/pages/RestaurantListPage/index.tsx
@@ -1,20 +1,56 @@
+import axios from 'axios';
 import { H1 } from 'components';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useRef, useState } from 'react';
+import { GlobalContext } from 'store';
 import styled from 'styled-components';
 
 const RestarauntListPage: React.FC = () => {
+  const { state, dispatch } = useContext(GlobalContext);
   const isMounted = useRef(true);
   const [isLoading, setIsLoading] = useState(true);
   const [restaurants, setRestaurants] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
+    if (isMounted.current) {
+      const source = axios.CancelToken.source();
+      const cancelToken = source.token;
+      const apiKey = process.env.REACT_APP_ZOMATO_API_KEY;
+      const config = {
+        cancelToken,
+        headers: {
+          'user-key' : apiKey
+        },
+      };
+      axios.get(`https://developers.zomato.com/api/v2.1/`, config)
+        .then(({ data }) => {
+          console.log(data);
+          setIsLoading(false);
+          source.cancel();
+        })
+        .catch(() => {
+          setErrorMessage('There was a problem getting cuisines, just type in your own!');
+          setIsLoading(false);
+          source.cancel();
+      })
+    }
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  const homePageOnClickHandler = useCallback(() => {
 
   }, []);
+
+  if (isLoading) {
+    return <H1>Loading</H1>
+  }
 
   return (
     <PageContainer>
       <Header>
-        <H1>Recommended Restaurants</H1>
+        <H1>{errorMessage.length > 0 ? errorMessage : 'Recommended Restaurants'}</H1>
       </Header>
       <MainContent>
 
